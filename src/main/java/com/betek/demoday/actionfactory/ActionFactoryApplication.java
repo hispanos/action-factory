27d5b12c65da0767f707e32e23c409e5af4e1735
Mailer Martinez <mailermartinez@gmail.com>
2024-04-02 11:33:13 -0500

    initial project

---
+++ src/main/java/com/betek/demoday/actionfactory/ActionFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.betek.demoday.actionfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betek/demoday/actionfactory/ActionFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betek.demoday.actionfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
