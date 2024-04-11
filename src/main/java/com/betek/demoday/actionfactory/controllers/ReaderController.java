137cec0647b36099001d5407fdca9359607a5eba
eliasib <ecantor.2906@gmail.com>
2024-04-10 20:16:17 -0500

    reader only part 1

--- src/main/java/com/betek/demoday/actionfactory/controllers/ReaderController.java
+++ src/main/java/com/betek/demoday/actionfactory/controllers/ReaderController.java
@@ -1,34 +1,42 @@
 package com.betek.demoday.actionfactory.controllers;
 
-import com.betek.demoday.actionfactory.models.ReaderRequest;
-import com.betek.demoday.actionfactory.models.responses.ResponseReader;
 import com.betek.demoday.actionfactory.services.readerService.ReaderService;
+import com.opencsv.CSVReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
 @RestController
 @RequestMapping("/file")
 public class ReaderController {
-    private final ReaderService readerService;
+    private ReaderService readerService;
 
     @Autowired
     public ReaderController(ReaderService readerService){
         this.readerService = readerService;
     }
 
-    @PostMapping
-    public ResponseEntity<ResponseReader> loadFile(@RequestBody ReaderRequest request) {
-        return ResponseEntity.ok(readerService.processFile(request.getUrlFile()));
+
+    @PostMapping("/upload-csv")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Por favor selecciona un archivo para subir.");
+            return "redirect:/";
+        }
+
+        try {
+            readerService.processCSVFile(file);
+            redirectAttributes.addFlashAttribute("message", "¡Archivo subido y procesado correctamente!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            redirectAttributes.addFlashAttribute("message", "¡Error al procesar el archivo CSV!");
+        }
+
+        return "redirect:/";
     }
-
-    @PostMapping("/upload")
-    public String load(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
-
-
-        return "Archivo subido.";
-    }
-
 }
+
+
